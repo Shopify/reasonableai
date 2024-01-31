4ef39c527535f785c253bd09d804d1096e0a8c0f
Greg Clarke <greg.clarke@shopify.com>
2024-01-31 16:32:26 -0500

    updating the app host

--- semantic_network/app.py
+++ semantic_network/app.py
@@ -182,4 +182,4 @@ def delete_all():
     return jsonify({"message": "All nodes and relationships deleted successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
