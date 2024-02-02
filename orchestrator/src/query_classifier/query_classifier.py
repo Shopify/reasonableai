68757100b99daab6094592d4b0a6b5d53717f64e
Greg Clarke <greg.clarke@shopify.com>
2024-02-02 16:21:17 -0500

    using load settings in query classifier

--- orchestrator/src/query_classifier/query_classifier.py
+++ orchestrator/src/query_classifier/query_classifier.py
@@ -1,18 +1,11 @@
 from jinja2 import Environment, FileSystemLoader
-from ..models.ability import Ability
-from ..models.semantic_network import SemanticNetwork
 from ..utils.query_llm import QueryLLM
+from ..utils.load_settings import SEMANTIC_NETWORKS, ABILITIES
 
 class QueryClassifier:
     def __init__(self, user_query):
         self.user_query = user_query
-        self.compentencies = [
-            Ability("GitHub", "Read GitHub repos, create pull requests and issues", "http://github.com"),
-            Ability("Researcher", "Search the web information, returns appropriate documents. Useful for finding facts", "http://google.com"),
-            SemanticNetwork("Tigers", "Information about Tigers, their habits, ranges, diets, etc", "http://tigers.com"),
-            SemanticNetwork("Lions", "Information about Lions, their habits, ranges, diets, etc", "http://lions.com"),
-            SemanticNetwork("Cats", "Information about house cats, their habits, naps, diets, etc", "http://cats.com")
-        ] #this will be replaced with a global variable or model
+        self.compentencies = SEMANTIC_NETWORKS + ABILITIES
 
     def classify(self):
         prompt_env = Environment(loader=FileSystemLoader('prompts'))
