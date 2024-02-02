929458ba357921b43b65eb614efe4e8432488dc2
Greg Clarke <greg.clarke@shopify.com>
2024-02-02 14:11:14 -0500

    classifying messages via chat

--- orchestrator/tasks/celery_app.py
+++ orchestrator/tasks/celery_app.py
@@ -1,9 +1,11 @@
 from celery import Celery
 from dotenv import load_dotenv
 import os
+# from .src.utils.competencies import Competencies
 
 load_dotenv()
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+# COMPENTENCIES = Competencies()
 
 app = Celery('tasks', broker=RABBITMQ_HOST)
--- orchestrator/tasks/chat.py
+++ orchestrator/tasks/chat.py
@@ -1,17 +1,24 @@
 import time
 import requests
 from .celery_app import app
+from src.query_classifier.query_classifier import QueryClassifier
 
 def process_incoming_message(message):
     print('Processing incoming message: {}'.format(message))
-    time.sleep(2)
-    # Enqueue the post_message_to_chat task
-    app.send_task('post_message_to_chat', kwargs={'message': 'hello world'})
+    print('Classifying message...')
+    query_classification = QueryClassifier(message).classify()
+    print(f'Classification results: {query_classification}')
+    most_relevant_topic = query_classification[0]
+    message = f"The most relevant topic is: {most_relevant_topic['topic name']}"
+    print(f"Posting message to chat: {message}")
+    app.send_task('post_message_to_chat', kwargs={'message': message})
 
 def post_message_to_chat(message):
     # there is a much better way to do this but it's getting late on Hack Days
+    print(f"Recieved message to post to chat: {message}")
     url = "http://127.0.0.1:5000/receive_bot_message"
     data = {'message': message}
+    print(f"Posting message to chat: {message}")
     response = requests.post(url, data=data)
     if response.status_code == 200:
         print(f"Message posted successfully: {message}")
